7d1288ed53b13adaaf7cacbc03c0cd6690f563bb
jihad <codebyjihad@gmail.com>
2026-01-04 15:54:16 +0600

    create Navbar

--- frontend/src/layout/Root.jsx
+++ frontend/src/layout/Root.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Root = () => {
   return (
     <>
-      <h1>Navbar</h1>
-      <Outlet/>
-      <h1>Footer</h1>
+      <Navbar/>
+       <main className='min-h-[calc(100vh-100px)]'>
+          <Outlet/>
+       </main>
+      <Footer/>
     </>
   )
 }
--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../layout/Root";
-import Home from "../components/Home";
+import Home from "../pages/Home";
+import Books from "../pages/Books";
+import Ebooks from "../pages/Ebooks";
+import MemberShip from "../pages/MemberShip";
+import Addbooks from "../pages/Addbooks";
 
 const router = createBrowserRouter([
     {
@@ -11,6 +15,22 @@ const router = createBrowserRouter([
             {
                 index:true,
                 element:<Home/>
+            },
+            {
+                path:'/books',
+                element:<Books/>
+            },
+            {
+                path:'/ebooks',
+                element:<Ebooks/>
+            },
+            {
+                path:'/membership',
+                element:<MemberShip/>
+            },
+            {
+                path:'/books/add',
+                element:<Addbooks/>
             }
         ]
     }
