bb4a66db5496060b091dd1bde07e340f7d737288
jihad <codebyjihad@gmail.com>
2026-01-10 13:44:02 +0600

    fix: update book display message format in Shop component

--- frontend/src/pages/shop/Shop.jsx
+++ frontend/src/pages/shop/Shop.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useBooks } from '../../context/BookContext'
 import BookGrid from './BookGrid'
+import CategoryNav from './CategoryNav'
+import SortBook from './SortBook'
 
 const Shop = () => {
 
@@ -9,21 +11,52 @@ const Shop = () => {
         loading,
         error,
         filters,
-        pagination, } = useBooks()
+        pagination, fetchBooks, updateFilters } = useBooks()
 
-        const handleDeleteBook = () => {
-            console.log('Boook deleted')
-        }
+    const categories = ['All Collctions', 'Fiction', 'Adventure', 'Romance', 'Dystopian', 'Histirical', 'Non-Fiction']
+
+    useEffect(() => {
+        fetchBooks()
+    }, [filters, fetchBooks])
+
+    const handleCatrgoryChange = (category) => {
+        updateFilters({
+            genre: category === 'All Collctions' ? '' : category,
+            page: 1
+        })
+    }
+
+    const handleSortChange = (sortConfig) => {
+        updateFilters({
+            sortBy: sortConfig.sortBy,
+            order: sortConfig.order,
+            page:1
+        })
+    }
+
+    const handleDeleteBook = () => {
+        console.log('Boook deleted')
+    }
 
     return (
-        <div className='container mx-auto'>
+        <div className='container mx-auto px-4 py-12 min-h-screen'>
+
+            <div className='flex justify-between items-center flex-wrap border-b border-gray-200'>
+                <CategoryNav activeCategory={filters.genre || "All Collctions"} categories={categories} onCategoryChange={handleCatrgoryChange} />
+                <div className='py-4 justify-end px-4'>
+                    <SortBook  currentSort={{
+                        sortBy: filters.sortBy,
+                        order: filters.order
+                    }}  onSortChange={handleSortChange}/>
+                </div>
+            </div>
             <div>
-               Showing {pagination.totalBook > 0 ? (pagination.currentPage - 1) * filters.limit  + 1: 0} of 10 books
+                Showing {pagination.totalBook > 0 ? (pagination.currentPage - 1) * filters.limit + 1 : 0} - <span>{Math.min(pagination.currentPage * filters.limit, pagination.totalBook)}</span> of {pagination.totalBook}
             </div>
             {/* books card */}
             <div className='py-8 md:px-8 '>
-                <BookGrid books={books} loading={loading} error={error} onDeleteBook={handleDeleteBook}/>      
-            </div>           
+                <BookGrid books={books} loading={loading} error={error} onDeleteBook={handleDeleteBook} />
+            </div>
         </div>
     )
 }
